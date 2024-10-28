22eb800f39eb7a17295b8fdb605e8d9136b86531
LucaW <lucawlodarczyk772@gmail.com>
2024-10-28 16:09:09 -0300

    Eyelit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import RecipeList from './pages/RecipeList'; 
+import RecipeDetails from './pages/RecipeDetail'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<RecipeList />} />
+      <Route path="/recipe/:id" element={<RecipeDetails />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
